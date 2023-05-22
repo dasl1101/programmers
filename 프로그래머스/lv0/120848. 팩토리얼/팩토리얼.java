c5523fc250d6428b8acb1842272d754a60cbd4f6
dasl1101 <102366257+dasl1101@users.noreply.github.com>
2023-05-22 10:57:11 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int sum = 1; 
+        for(int i=1 ; i<=n; i++){
+            sum *= i;
+            if(sum==n){
+                answer = i;
+                break;
+            }
+            if(sum >n){
+                answer = i-1; 
+                break;
+                
+            }
+        }
+        
+        
+        return answer;
+    }
+}
