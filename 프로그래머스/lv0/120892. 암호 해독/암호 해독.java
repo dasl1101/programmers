92435d0f84765be568a02e6e49be9c36976e554d
dasl1101 <102366257+dasl1101@users.noreply.github.com>
2023-05-30 11:21:17 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,6 @@
-import java.util.*;
 class Solution {
     public String solution(String cipher, int code) {
-        StringBuilder answer = new StringBuilder();
-        char[] arr = cipher.toCharArray();
-        for (int i = code - 1; i < arr.length; i += code) {
-            answer.append(arr[i]);
-        }
-        return answer.toString();
+        String answer = "";
+        return answer;
     }
 }
