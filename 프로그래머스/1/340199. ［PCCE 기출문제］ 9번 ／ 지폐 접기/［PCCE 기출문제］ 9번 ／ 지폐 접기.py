d38d7d1935598dc00e7bdd5b262896c57c31d911
dasl1101 <102366257+dasl1101@users.noreply.github.com>
2024-10-30 13:55:45 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -0,0 +1,13 @@
+def solution(wallet, bill):
+    answer = 0
+    wallet.sort()
+    bill.sort()
+    while wallet[0] < bill[0] or wallet[1] < bill[1]:
+        if bill[0] > bill[1]:
+            bill[0] = bill[0]//2
+        else:
+            bill[1] = bill[1]//2
+        bill.sort()
+        answer += 1
+        
+    return answer
